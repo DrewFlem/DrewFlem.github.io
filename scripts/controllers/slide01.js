7c8b6347b9dfd4c3a8ad75fad4d697914d2dac0e
DrewFlem <dadrew1@gmail.com>
2015-11-02 16:31:24 -0500

    initial commit

---
+++ scripts/controllers/slide01.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name slideshowApp.controller:Slide01Ctrl
+ * @description
+ * # Slide01Ctrl
+ * Controller of the slideshowApp
+ */
+angular.module('slideshowApp')
+  .controller('Slide01Ctrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
